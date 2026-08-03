23fd441e11a35c86b59d11c0fcb4fb3ac2d8e13e
Miguel Angel <rmiguelangel222@gmail.com>
2026-08-03 06:00:02 +0200

    Creacion y finalizacion de reloj estilo apple

---
+++ script.js
@@ -0,0 +1,12 @@
+function actualizarTiempo(){
+    const tiempo = new Date();
+    const horas = String(tiempo.getHours()).padStart(2, '0');
+    const minutos = String(tiempo.getMinutes()).padStart(2, '0');
+    const segundos = String(tiempo.getSeconds()).padStart(2, '0');
+
+    document.getElementById('reloj').textContent = `${horas}:${minutos}:${segundos}`;
+}
+
+actualizarTiempo();
+setInterval(actualizarTiempo, 1000);
+
