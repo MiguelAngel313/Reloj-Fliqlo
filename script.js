fb68f1b3666f2819e030f96f19fbaf46dfcba471
Miguel Angel <rmiguelangel222@gmail.com>
2026-08-17 19:52:16 +0200

    Proyecto rersponsivo y completamente terminado.

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ function actualizarTiempo(){
     const minutos = String(tiempo.getMinutes()).padStart(2, '0');
     const segundos = String(tiempo.getSeconds()).padStart(2, '0');
 
-    document.getElementById('reloj').textContent = `${horas}:${minutos}:${segundos}`;
+    //document.getElementById('reloj').textContent = `${horas}:${minutos}:${segundos}`;
+
+    document.getElementById('horas').textContent = `${horas}`;
+    document.getElementById('minutos').textContent = `${minutos}`;
+    document.getElementById('segundos').textContent = `${segundos}`;
 }
 
 actualizarTiempo();
